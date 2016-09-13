aaa88629a6a91afd9edc0a7619aa61fda9dea2d3
Administrator <Administrator@2013-20160906CA>
2016-09-13 15:02:09 +0800

    统计

---
+++ web/templates/default/layout/baiduTj.php
@@ -0,0 +1,10 @@
+<?php defined('InShopNC') or exit('Access Invalid!');?>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?4c065fbe95d831af558745321320b830";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
